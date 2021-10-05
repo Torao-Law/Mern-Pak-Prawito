3a19cd1babc45a96f158dc8fd0ace94c17127417
Dandi Saputra <67894946+Torao-Law@users.noreply.github.com>
2021-10-05 11:16:15 +0700

    Mengganti data dummy berdasarkan respon api

--- src/Components/molecules/BlogItem/index.js
+++ src/Components/molecules/BlogItem/index.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import { Button, Gap } from '../..'
-import { RegisterBg } from '../../../assets'
 import './blogitem.scss'
 import { useHistory } from 'react-router'
 
-const BlogItem = () => {
+const BlogItem = (props) => {
     const history = useHistory();
+    const {name, title, body, date, image} = props;
 
     return (
         <div className="blog-item">
-            <img className="image-thumb" src={RegisterBg} alt="post" />
+            <img className="image-thumb" src={image} alt="post" />
             <div className="content-detail">
-                <p className="title">Make with React Js</p>
-                <p className="author">Author - post</p>
-                <p className="body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo voluptatum excepturi minima nesciunt ipsum veniam, quas magni commodi quaerat iste quis corporis, error molestiae recusandae iure officiis ab a aperiam.</p>
+                <p className="title">{title}</p>
+                <p className="author">{name} - {date}</p>
+                <p className="body">{body}</p>
                 <Gap height={20}/>
                 <Button title="Read More" onClick={() => history.push('/detail-blog')} />
             </div>
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,15 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {BlogItem, Button, Gap} from '../../Components'
 import './home.scss'
 import { useHistory } from 'react-router'
 import Axios from 'axios'
 
 const Home = () => {
+    // membuat state dengan nama dataBlog dan methode setDataBlog
+    const [dataBlog, setDataBlog] = useState([]) //useState harus memiliki value, disini diisi array kosong untuk menerima respon API
     // function untuk memanggil data api dari node js
     useEffect(() => {
         Axios.get('http://localhost:4000/v1/blog/posts') //memanggil data menggunakan library axios dengan method get dari nodeJS
         .then(result => {
             console.log('Data API :', result.data) //jika fungsi pemanggilan berhasil
+            const responseAPI = result.data; //variabel untuk menganti dataBLog
+
+            setDataBlog(responseAPI.data) //cara memanggil variable untuk mengganti dataBlog menggunakan methode setDataBlog
         })
         .catch(err => {
             console.log('err', err) //jika fungsi pemanggilan error
@@ -25,10 +30,10 @@ const Home = () => {
             </div>
             <Gap height={20}/>
             <div className="content-wrapper">
-                <BlogItem/>
-                <BlogItem/>
-                <BlogItem/>
-                <BlogItem/>
+                {dataBlog.map(blog => {
+                        return <BlogItem key={blog._id} image={`http://localhost:4000/${blog.image}`} title={blog.title} body={blog.body} author={blog.author.name} date={blog.createdAt}/>
+                    })
+                }
             </div>
             <div className="pagination">
                 <Button title="Previous" />
