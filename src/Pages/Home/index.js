a6882be2050210fa3ec3c3f910965585c305aa8c
Dandi Saputra <67894946+Torao-Law@users.noreply.github.com>
2021-10-06 10:22:11 +0700

    change value state global redux

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,29 +3,36 @@ import {BlogItem, Button, Gap} from '../../Components'
 import './home.scss'
 import { useHistory } from 'react-router'
 import Axios from 'axios'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Home = () => {
     // membuat state dengan nama dataBlog dan methode setDataBlog
-    const [dataBlog, setDataBlog] = useState([]) //useState harus memiliki value, disini diisi array kosong untuk menerima respon API
+    // const [dataBlog, setDataBlog] = useState([]) //useState harus memiliki value, disini diisi array kosong untuk menerima respon API
+    const dispatch = useDispatch();
 
     // menggunakan state global yang telah di setting pada redux/store
-    const stateGlobal = useSelector(state => state)
-    console.log("State Global : ", stateGlobal)
+    const {dataBlogs, name} = useSelector(state => state)
+    // console.log("State Global : ", stateGlobal)
+    console.log("State global : ", dataBlogs)
 
     // function untuk memanggil data api dari node js
     useEffect(() => {
+        setTimeout(() => {
+            dispatch({type: 'UPDATE_NAME'})
+        }, 3000)
+
         Axios.get('http://localhost:4000/v1/blog/posts') //memanggil data menggunakan library axios dengan method get dari nodeJS
         .then(result => {
             console.log('Data API :', result.data) //jika fungsi pemanggilan berhasil
             const responseAPI = result.data; //variabel untuk menganti dataBLog
-
-            setDataBlog(responseAPI.data) //cara memanggil variable untuk mengganti dataBlog menggunakan methode setDataBlog
+            //cara memanggil state lokal variable untuk mengganti dataBlog menggunakan methode setDataBlog
+            // setDataBlog(responseAPI.data)
+            dispatch({type: 'UPDATE_DATA_BLOG', payload: responseAPI.data}) //cara memanggil state global
         })
         .catch(err => {
             console.log('err', err) //jika fungsi pemanggilan error
         })
-    }, [])
+    }, [dispatch])
 
     const history = useHistory();
 
@@ -34,9 +41,10 @@ const Home = () => {
             <div className="create-wrapper">
                 <Button title="Create Blog" onClick={() => history.push('/create-blog')}/>
             </div>
+            <p>{name}</p>
             <Gap height={20}/>
             <div className="content-wrapper">
-                {dataBlog.map(blog => {
+                {dataBlogs.map(blog => {
                         return <BlogItem key={blog._id} image={`http://localhost:4000/${blog.image}`} title={blog.title} body={blog.body} name={blog.author.name} date={blog.createdAt}/>
                     })
                 }
--- src/config/redux/store.js
+++ src/config/redux/store.js
@@ -1,13 +1,28 @@
 import { createStore } from "redux";
 
 const initialState = {
-    dataBlog: [],
+    dataBlogs: [],
     name: "Dandi Saputra"
 }
 
 // sebuah function yang mengirimkan value yg sering disebut state global yang akan diterima store, dan menerima 2 parameter utama yaitu (state & action)
 // state disini yang akan menjadi state global yang dimana valuenya adalah initial state
 const reducer = (state = initialState, action) => {
+    // merubah value dari state global redux
+    // kita menggunakan logic perbandingan untuk action
+    // action.type adalah kata kunci untuk melakukan perubahan yang diingikan
+    if(action.type === 'UPDATE_DATA_BLOG') {
+        return {
+            ...state,
+            dataBlogs: action.payload
+        }
+    }
+    if(action.type === 'UPDATE_NAME') {
+        return {
+            ...state,
+            name: "Dandi"
+        }
+    }
     return state
 }
 
